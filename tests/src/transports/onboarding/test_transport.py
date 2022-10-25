a0e798ed89c3cd29e245816f62053c077a7d35d8
Samuel V M <samuel.marcal@ligainvest.com.br>
2022-10-25 18:03:55 -0300

    feat: validate if antifraud is reproved

--- tests/src/transports/onboarding/test_transport.py
+++ tests/src/transports/onboarding/test_transport.py
@@ -1,27 +1,77 @@
 from func.src.domain.exceptions.exceptions import OnboardingStepsStatusCodeNotOk
 from func.src.transports.onboarding_steps.transport import OnboardingSteps
-from tests.src.transports.onboarding.stubs import stub_request_success, stub_request_failure
 from decouple import AutoConfig
-
-from unittest.mock import patch
+from httpx import AsyncClient
+from unittest.mock import patch, MagicMock
 
 import pytest
 
+from func.src.domain.models.onboarding import Onboarding
+
+
+dummy_value = MagicMock()
+
+
+def raise_second(*args):
+    if isinstance(args[1], Exception):
+        raise args[1]
+
 
 @pytest.mark.asyncio
 @patch.object(AutoConfig, "__call__")
-@patch('func.src.transports.onboarding_steps.transport.AsyncClient.get', return_value=stub_request_success)
-async def test_when_success_to_get_onboarding_steps_then_returns_current_step(mock_httpx_client, mocked_env):
-    user_current_step = await OnboardingSteps.get_user_current_step(jwt='12345')
-
-    assert isinstance(user_current_step, str)
-    assert user_current_step == "selfie"
+@patch.object(AsyncClient, "__init__", return_value=None)
+@patch.object(AsyncClient, "__aenter__")
+@patch.object(AsyncClient, "__aexit__", side_effect=raise_second)
+@patch.object(Onboarding, "__init__", return_value=None)
+async def test_when_success_to_get_onboarding_steps_then_returns_current_step(
+        mocked_model,
+        mocked_client_exit,
+        mocked_client_enter,
+        mocked_client_instance,
+        mocked_env
+):
+    mocked_client_enter.return_value.get.return_value = MagicMock()
+    mocked_client_enter.return_value.get.return_value.status_code = 200
+    await OnboardingSteps.get_user_current_step(jwt=dummy_value)
+    mocked_client_enter.return_value.get.assert_called_once_with(
+        mocked_env.return_value, headers={"x-thebes-answer": dummy_value}
+    )
+    mocked_model.assert_called_once_with(
+        step=(
+            mocked_client_enter.return_value
+            .get.return_value
+            .json.return_value
+            .get.return_value
+            .get.return_value
+        ),
+        anti_fraud=(
+            mocked_client_enter.return_value
+            .get.return_value
+            .json.return_value
+            .get.return_value
+            .get.return_value
+        ),
+    )
 
 
 @pytest.mark.asyncio
 @patch.object(AutoConfig, "__call__")
-@patch('func.src.transports.onboarding_steps.transport.AsyncClient.get', return_value=stub_request_failure)
-async def test_when_failure_to_get_onboarding_steps_then_raises(mock_httpx_client, mocked_env):
+@patch.object(AsyncClient, "__init__", return_value=None)
+@patch.object(AsyncClient, "__aenter__")
+@patch.object(AsyncClient, "__aexit__", side_effect=raise_second)
+@patch.object(Onboarding, "__init__", return_value=None)
+async def test_when_success_to_get_onboarding_steps_then_raises(
+        mocked_model,
+        mocked_client_exit,
+        mocked_client_enter,
+        mocked_client_instance,
+        mocked_env
+):
+    mocked_client_enter.return_value.get.return_value = MagicMock()
+    mocked_client_enter.return_value.get.return_value.status_code = 500
     with pytest.raises(OnboardingStepsStatusCodeNotOk):
-        await OnboardingSteps.get_user_current_step(jwt='12345')
-
+        await OnboardingSteps.get_user_current_step(jwt=dummy_value)
+    mocked_client_enter.return_value.get.assert_called_once_with(
+        mocked_env.return_value, headers={"x-thebes-answer": dummy_value}
+    )
+    mocked_model.assert_not_called()
